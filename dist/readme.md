a043665f71f51b097f6bebee887257e20e2d0dea
Lauren Groh <legroh@uwm.edu>
2021-10-24 11:56:23 -0500

    fix license functions, finish fs.writeFile, test app

---
+++ dist/readme.md
@@ -0,0 +1,46 @@
+# d
+
+  ## Description 
+   adsfadsf
+  
+  ## Badges
+  https://shields.io/badge/license-GPL License-blue
+  
+* [License](#license)
+
+  
+  ## Table of Contents 
+  * [Installation](#installation)
+  * [Instructions](#instructions)
+  * [Tests](#tests)
+  * [Languages](#languages)
+  * [Licenses](#license)
+  * [Contributing](#contributing)
+  * [Credits](#credits)
+  * [Questions](#questions)
+
+  
+  
+  ## Installation
+  adsfadsf
+  
+  ## Instructions 
+  adsf
+  
+  ## Tests
+  adsf
+
+  ## Languages
+  HTML,JavaScript,Node.js
+
+  ## License
+  GPL License
+  
+  ## Contributing
+  dsf
+
+  ## Credits
+  adsf
+
+  ## Questions
+  If you have any questions please connect with me, here ![email](github.com/grohtech) or email me, here ![GitHub](legroh@uwm.edu).
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // App packages 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { generateMarkdown } = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // const { rejects } = require('assert');
 // const { resolve } = require('path');
 
@@ -13,10 +13,10 @@ const questions = [
         message: 'What is the title of your project? (Required)',
         validate: titleInput => {
             if (titleInput) {
-              return true;
+                return true;
             } else {
-              console.log('Please enter the title of your project!');
-              return false;
+                console.log('Please enter the title of your project!');
+                return false;
             }
         }
     },
@@ -26,25 +26,25 @@ const questions = [
         message: 'Please describe your project. (Required)',
         validate: descriptionInput => {
             if (descriptionInput) {
-              return true;
+                return true;
             } else {
-              console.log('Please enter a description!');
-              return false;
+                console.log('Please enter a description!');
+                return false;
             }
         }
     },
-        {
+    {
         type: 'input',
         name: 'installation',
         message: 'What do your users need to install use this app? (Required)',
         validate: installationInput => {
             if (installationInput) {
-              return true;
+                return true;
             } else {
-              console.log('Your users would appreciate installation instructions!');
-              return false;
+                console.log('Your users would appreciate installation instructions!');
+                return false;
             }
-        }    
+        }
     },
     {
         type: 'input',
@@ -52,12 +52,12 @@ const questions = [
         message: 'What instructions do your users need to use this app? (Required)',
         validate: instructionsInput => {
             if (instructionsInput) {
-              return true;
+                return true;
             } else {
-              console.log('Your users would appreciate instructions!');
-              return false;
+                console.log('Your users would appreciate instructions!');
+                return false;
             }
-        }    
+        }
     },
     {
         type: 'input',
@@ -83,10 +83,10 @@ const questions = [
         message: 'What are the requirements for contributing to your project? (Required)',
         validate: contributingInput => {
             if (contributingInput) {
-              return true;
+                return true;
             } else {
-              console.log('If there are no requirements to contribute, type "none". If there are contributions allowed, type "no contributions allowed".');
-              return false;
+                console.log('If there are no requirements to contribute, type "none". If there are contributions allowed, type "no contributions allowed".');
+                return false;
             }
         }
     },
@@ -96,10 +96,10 @@ const questions = [
         message: 'Who contributed to your project? (Required)',
         validate: creditsInput => {
             if (creditsInput) {
-              return true;
+                return true;
             } else {
-              console.log('Please give some credit to the contributors!');
-              return false;
+                console.log('Please give some credit to the contributors!');
+                return false;
             }
         }
     },
@@ -109,10 +109,10 @@ const questions = [
         message: 'What is your personal Github link? (Required)',
         validate: githubInput => {
             if (githubInput) {
-              return true;
+                return true;
             } else {
-              console.log('Give users a way to look at your other work!');
-              return false;
+                console.log('Give users a way to look at your other work!');
+                return false;
             }
         }
     },
@@ -122,46 +122,45 @@ const questions = [
         message: 'What is your email address? (Required)',
         validate: emailInput => {
             if (emailInput) {
-              return true;
+                return true;
             } else {
-              console.log('Give users a way to contact you!');
-              return false;
+                console.log('Give users a way to contact you!');
+                return false;
             }
         }
     },
-  ]
+]
 
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
 
 
 // TODO: Create a function to write README file
 
-function writeToFile = fileContent => {
-    
-    fs.writeFile('./dist/readme.md'), fileContent, err => {
-        if (err) {
-            rejects(err);
-            return;
+function writeToFile(answers) {
+
+    fs.writeFile('./dist/readme.md', generateMarkdown(answers)
+        , err => {
+            if (err) {
+                console.error(err)
+                return
+            }
         }
-        resolve({
-            ok: true,
-            message: 'Abrakadabra README.md!'
-        });
-    }
+    )
 };
 
 // Initialize app
 function init() {
     inquirer.prompt(questions)
-    .then((answers) => {
-  console.log(answers);
-    })
+        .then((answers) => {
+            writeToFile(answers)
+            console.log(answers);
+        })
 }
 init();
